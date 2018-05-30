0ab0492b1874cb598b324a8f7ab5f62d3fc857a5
Ghwolf <g470756541@gmail.com>
2018-05-30 22:27:00 +0800

    刷新服务器缓存

---
+++ app/Http/Controllers/Api/RefreshCache.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use DB;
+use Cache;
+use App\Models\GameServer;
+use Illuminate\Http\Request;
+
+class RefreshCache extends Controller
+{
+    public function refresh(Request $request)
+    {
+    	Cache::forget('serverList');
+    	GameServer::serverList();
+
+    	Cache::forget('visited_serverlist_');
+    	Cache::forget('visited_serverlist_1');
+
+    	Cache::forget('serverData');
+    	GameServer::serverData();
+
+    	Cache::forget('serverAllData');
+    	GameServer::serverAllData();
+
+    	Cache::forget('recommendSid');
+    	GameServer::recommend();
+
+    	Cache::forget('t_server_new_');
+    	$newServer = Cache::remember('t_server_new_', config('cache.expires'), function () {
+            return DB::table('t_server')->where([['gameid', '<', 9999], 'status' => 'online'])->select('gameid')->orderBy('gameid', 'desc')->first();
+        });
+
+    	$serverid = $request->serverid;
+    	if ($serverid) {
+    		Cache::forget('t_server_' . $serverid);
+	        Cache::remember('t_server_' . $serverid, config('cache.expires'), function () use ($serverid) {
+	            return DB::table('t_server')->where('gameid', $serverid)->select('ip', 'gmport')->first();
+	        });
+
+    	}
+
+        return ['success' => 'true'];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,7 @@ Route::group([
 
 	// 服务器选择
 	Route::get('userSelectGameServer', 'UserSelectGameServer@index');
+
+	// 新增服务器
+	Route::get('refreshServer', 'RefreshCache@refresh');
 });
