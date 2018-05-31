c5251644c4080ce403fd0a775daf88c04370e01f
Ghwolf <g470756541@gmail.com>
2018-05-31 19:41:39 +0800

    旌旗新渠道

--- config/ChannelParam/kingcheer.php
+++ config/ChannelParam/kingcheer.php
@@ -8,5 +8,11 @@ return [
             "appkey" => "95a7ac922c7a4cbaa61af1bd823a4007",
             "paykey" => "d273483b1ae94048ad25292397f0c3cf",
         ],
+        // 逗妖传
+        "jingqiADYZ" => [
+            "appid" => "20240",
+            "appkey" => "dafd033123734b4d8622eabaee6a9757",
+            "paykey" => "90707fa382cc4187bfc3b64d3dd86072",
+        ],
     ],
 ];
