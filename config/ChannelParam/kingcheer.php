5255d0037bfce6de8fe85b15f814e1044ebb1417
Ghwolf <g470756541@gmail.com>
2018-06-04 10:39:22 +0800

    妖妖灵渠道添加

--- config/ChannelParam/kingcheer.php
+++ config/ChannelParam/kingcheer.php
@@ -14,5 +14,11 @@ return [
             "appkey" => "dafd033123734b4d8622eabaee6a9757",
             "paykey" => "90707fa382cc4187bfc3b64d3dd86072",
         ],
+        // 妖妖灵
+        "jingqiADYZ" => [
+            "appid" => "20246",
+            "appkey" => "27c0bd39a4dc45869bd90ee0be0fc300",
+            "paykey" => "753729061569412da6f93d14fac539bf",
+        ],
     ],
 ];
