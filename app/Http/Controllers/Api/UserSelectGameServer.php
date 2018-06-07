ee2b9efd89033021159f1352882d5dc07d728ecb
Ghwolf <g470756541@gmail.com>
2018-06-07 20:14:28 +0800

    老服上线

--- app/Http/Controllers/Api/UserSelectGameServer.php
+++ app/Http/Controllers/Api/UserSelectGameServer.php
@@ -42,8 +42,8 @@ class UserSelectGameServer extends Controller
             $newServer = Cache::remember('t_server_new_', config('cache.expires'), function () use ($serverid) {
                 return DB::table('t_server')->where([['gameid', '<', 9999], 'status' => 'online'])->select('gameid')->orderBy('gameid', 'desc')->first();
             });
-            $bInWhiteList = checkWhite();
-            if ($bInWhiteList) {
+            // $bInWhiteList = checkWhite();
+            // if ($bInWhiteList) {
                 // 防止玩家登陆老服
                 $vistidList = cache('visited_' . $uid);
                 $serverList = [$newServer->gameid];
@@ -54,7 +54,7 @@ class UserSelectGameServer extends Controller
                 if (!in_array($serverid, $serverList)) {
                     return $this->responseResult('false', '服务器爆满.', ['errorcode' => 5]);
                 }
-            }
+            // }
             if (isset($newServer->gameid) && $newServer->gameid == $serverid) {
                 $online = Cache::remember('server_new_online_', \Carbon\Carbon::now()->addSeconds(5), function () {
                     try {
