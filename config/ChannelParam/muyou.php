b5cc65927fecf85179935a63e5734905b786429a
Ghwolf <g470756541@gmail.com>
2018-06-11 14:58:30 +0800

    安卓官包渠道添加

--- config/ChannelParam/muyou.php
+++ config/ChannelParam/muyou.php
@@ -3,25 +3,15 @@
 return [
     "LoginURL" => "http://sdk11.52muyou.com/sdk.php/LoginNotify/login_verify",
     "params" => [
-        "muyouandroid" => [
-            "appid"     => "1A27E3E04FF3C466B",
-            "gamekey"   => "swfewewefjiowef",
-            "secretkey" => "ChsYGh0CCQoXAg4D",
+        "muyoughqnxs" => [
+            "appid" => "B8494241B6AE3A0FA",
+            "gamekey" => "kyghqnxs",
+            "secretkey" => "FBcGHRkNHx0=",
         ],
-        "muyouios" => [
-            "appid"     => "C0F227773B5056AC2",
-            "gamekey"   => "fweodkwoeiwwfw",
-            "secretkey" => "ChsYGh0KCB0=",
-        ],
-        "muyouzyj" => [
-            "appid"     => "F5741D8A57CF88C6D",
-            "gamekey"   => "swfewewefjiowef",
-            "secretkey" => "ChsYGh0ZHgQMAhQ=",
-        ],
-        "muyoutgxm" => [
-            "appid"     => "5C9B2A31925F8D9F7",
-            "gamekey"   => "zuoyaojituiguangxm",
-            "secretkey" => "HRsODAkMDQcRGA4AGwAbDxsK",
+        "muyoutg" => [
+            "appid" => "AD26FBD230AD66E42",
+            "gamekey" => "zuoyaojituiguang",
+            "secretkey" => "HRsODAkMDQcRGA4AGwAbD0M=",
         ],
     ],
     'appid_muyou'        => "1A27E3E04FF3C466B",
