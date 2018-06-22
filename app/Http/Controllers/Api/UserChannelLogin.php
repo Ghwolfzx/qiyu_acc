d40dec74ba45bce028372dae930632bcd92824d1
Ghwolf <g470756541@gmail.com>
2018-06-22 10:11:26 +0800

    360

--- app/Http/Controllers/Api/UserChannelLogin.php
+++ app/Http/Controllers/Api/UserChannelLogin.php
@@ -121,6 +121,7 @@ class UserChannelLogin extends Controller
         	}
             if ($channelname == '_360') {
                 $channelname_fix = '360';
+                \Log::info('360 ====' . $uin . '@' . $channelname_fix);
             }
 
             // 账号查询
