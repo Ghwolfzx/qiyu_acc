7519579644b730b0e30985b7a3e8befe56c97959
Ghwolf <g470756541@gmail.com>
2018-06-30 14:17:36 +0800

    老服玩家规则

--- app/Http/Controllers/Api/UserSelectGameServer.php
+++ app/Http/Controllers/Api/UserSelectGameServer.php
@@ -47,6 +47,19 @@ class UserSelectGameServer extends Controller
             $newServer = Cache::remember('t_server_new_', config('cache.expires'), function () use ($serverid) {
                 return DB::table('t_server')->where([['gameid', '<', 9999], 'status' => 'online'])->select('gameid')->orderBy('gameid', 'desc')->first();
             });
+            $bInWhiteList = checkWhite();
+            if (!$bInWhiteList) {
+                // 防止玩家登陆老服
+                $vistidList = cache('visited_' . $uid);
+                $serverList = [$newServer->gameid, 999999, 1000000];
+                if (!empty($vistidList)) {
+                    $serverList = array_merge($serverList, $vistidList);
+                }
+
+                if (!in_array($serverid, $serverList)) {
+                    return $this->responseResult('false', '当前区服爆满，请前往最新区服。', ['errorcode' => 5]);
+                }
+            }
             if (isset($newServer->gameid) && $newServer->gameid == $serverid) {
                 $online = Cache::remember('server_new_online_', \Carbon\Carbon::now()->addSeconds(5), function () {
                     try {
